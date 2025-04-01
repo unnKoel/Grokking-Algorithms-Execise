5519f1c16b9f49f3f8913ca38cbbb8007e9b9fd4
unnkoel <unnkoel@gmail.com>
2025-04-01 20:57:09 +0800

    add binarySearch

--- src/4.js
+++ src/4.js
@@ -28,6 +28,24 @@ function max(arr) {
 4.4 Remember binary search from chapter 1? It’s a divide-and-conquer
 algorithm, too. Can you come up with the base case and recursive
 case for binary search?
-*/
 
-export { sum, count, max };
+arr.length / 2 is possible to be bigger than arr.length - 1?
+prove:  x / 2 > x - 1, when x < 2, that x = 0 or 1;
+        when x = 0, Math.floor(0/2) = 0 > x(0) - 1
+        when x = 1, Math.floor(1/2) = 0 === x(1) -1
+*/
+function binarySearch(arr, target, start, end) {
+  if (arr.length === 0) return -1;
+  if (arr.length === 1) {
+    return arr[0] === target ? 0 : -1;
+  }
+
+  end = end === undefined ? arr.length - 1 : end;
+  start = start === undefined ? 0 : start;
+  const mid = Math.floor((start + end) / 2);
+  if (target === arr[mid]) return mid;
+  if (target > arr[mid]) return binarySearch(arr, target, mid + 1, end);
+  if (target < arr[mid]) return binarySearch(arr, target, start, mid - 1);
+}
+
+export { sum, count, max, binarySearch };
--- test/4.test.js
+++ test/4.test.js
@@ -1,4 +1,4 @@
-import { sum, count, max } from "../src/4";
+import { sum, count, max, binarySearch } from "../src/4";
 
 test("D&Q sum check", () => {
   expect(sum([])).toBe(0);
@@ -18,4 +18,11 @@ test("D&Q count max", () => {
   expect(max([3])).toBe(3);
   expect(max([2, 3])).toBe(3);
   expect(max([2, 3, 6, 7])).toBe(7);
+});
+
+test("D&Q binary", () => {
+  expect(binarySearch([], 3)).toBe(-1);
+  expect(binarySearch([2], 3)).toBe(-1);
+  expect(binarySearch([3], 3)).toBe(0);
+  expect(binarySearch([2, 4, 7, 9, 10, 40, 43], 43)).toBe(6);
 });
